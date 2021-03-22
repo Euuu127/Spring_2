a9b93f6b70d8d6883bc4e0669075d24c2aa58ee5
Euuu127 <mak12cht7@gmail.com>
2021-03-22 15:18:21 +0900

    /

--- src/test/java/com/dkmk/s2/robot/RobotTest.java
+++ src/test/java/com/dkmk/s2/robot/RobotTest.java
@@ -11,16 +11,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
 public class RobotTest {
-	
 	@Autowired
 	private Robot robot;
-
+	//private Arm_Left arm_Left;
+	
 	@Test
 	public void test() {
-		//left_arm의 name
-		System.out.println(robot.getArm_Left().getName());
-		//right_arm의 name
-		System.out.println(robot.getArm_Right().getName());
+		
+		assertNotNull(robot.getArm_Right());
+		
+		
 	}
 
 }

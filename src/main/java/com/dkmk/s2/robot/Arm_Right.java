62f7289a680ed81f1d2323fbffba266adc290216
Euuu127 <mak12cht7@gmail.com>
2021-03-22 13:16:44 +0900

    n...

---
+++ src/main/java/com/dkmk/s2/robot/Arm_Right.java
@@ -0,0 +1,12 @@
+package com.dkmk.s2.robot;
+
+import org.springframework.stereotype.Component;
+
+@Component("ar")
+public class Arm_Right extends Arm {
+	
+	public Arm_Right() {
+		this.setName("Right Arm");
+	}
+
+}
---
+++ src/test/java/com/dkmk/s2/robot/RobotTest.java
@@ -0,0 +1,26 @@
+package com.dkmk.s2.robot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
+public class RobotTest {
+	
+	@Autowired
+	private Robot robot;
+
+	@Test
+	public void test() {
+		//left_arm의 name
+		System.out.println(robot.getArm_Left().getName());
+		//right_arm의 name
+		System.out.println(robot.getArm_Right().getName());
+	}
+
+}
